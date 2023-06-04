1255d7f4433e5fe45c8f38fb0668029dbd05c108
Oktavianus Bima Jadiva <oktavianus.bima@ti.ukdw.ac.id>
2023-06-04 15:15:14 +0700

    comit login

--- src/main/java/com/rentalmobil/rentalmobil/HelloController.java
+++ src/main/java/com/rentalmobil/rentalmobil/HelloController.java
@@ -6,12 +6,67 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloController {
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button loginButton;
+
+    public void toLogin(ActionEvent eventMenu) throws IOException {
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        // Lakukan validasi login di sini, misalnya dengan memeriksa username dan
+        // password di database
+        boolean loginSuccess = validateLogin(username, password);
+
+        if (loginSuccess) {
+            // Jika login sukses, buka halaman dashboard.fxml
+            toDashboard(eventMenu);
+        } else {
+            // Jika login gagal, tampilkan pesan kesalahan atau lakukan tindakan lainnya
+            System.out.println("Login gagal!");
+        }
+    }
+
+    private boolean validateLogin(String username, String password) {
+        // Lakukan validasi login, misalnya dengan memeriksa username dan password di
+        // database
+        return username.equals("admin") && password.equals("admin");
+    }
+
+    private void toDashboard(ActionEvent event) throws IOException {
+        Parent dashboard = FXMLLoader.load(getClass().getResource("dashboard.fxml"));
+        Scene dashboardScene = new Scene(dashboard);
+
+        // Mendapatkan stage/window saat ini
+        Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        // Set scene baru (dashboard.fxml)
+        currentStage.setScene(dashboardScene);
+        currentStage.show();
+    }
+
+    public void toAdminDashboard(ActionEvent eventMenu) throws IOException {
+        Parent toMenu = FXMLLoader.load(getClass().getResource("admin-dashboard.fxml"));
+        Scene sceneMenu = new Scene(toMenu);
+        Stage stageMenu = (Stage) ((Node) eventMenu.getSource()).getScene().getWindow();
+
+        stageMenu.setScene(sceneMenu);
+        stageMenu.show();
+    }
 
     public void toAdminDashboard(ActionEvent eventMenu) throws IOException {
         Parent toMenu = FXMLLoader.load(getClass().getResource("admin-dashboard.fxml"));
