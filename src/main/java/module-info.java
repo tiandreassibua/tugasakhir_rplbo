911118d5f682808e6fbef0b03b0a20fcc1466c63
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2023-06-05 18:54:27 +0700

    delete all file

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.rentalmobil.rentalmobil {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.rentalmobil.rentalmobil to javafx.fxml;
-    exports com.rentalmobil.rentalmobil;
-}
